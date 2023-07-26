49231d28e628654026d5bbb3d8d56d15f250490d
moulineE <elmahdi.mouline@live.fr>
2023-07-26 03:13:21 +0100

    Task 0. Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - a function that sorts an array of integers in
+ * ascending order using the Bubble sort algorithm
+ * @array: the array to sort
+ * @size: the size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	unsigned long int i;
+	int flag = 0;
+	unsigned long int fin = 0;
+
+	while (fin <= size - 1)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+				flag++;
+			}
+		}
+		if (flag == 0)
+		{
+			return;
+		}
+		else
+		{
+			flag = 0;
+		}
+	}
+}
