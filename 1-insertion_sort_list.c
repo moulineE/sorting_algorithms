4cc5c6521d66bb620312f6b6f264fa920c5135f6
moulineE <elmahdi.mouline@live.fr>
2023-07-30 02:49:21 +0100

    Task 1. Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,43 @@
 #include "sort.h"
+/**
+ * insertion_sort_list - a function that sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: a doubly linked list of integers
+ */
 
 void insertion_sort_list(listint_t **list)
 {
-	return;
+	listint_t *current = *list;
+	listint_t *tmp;
+
+	if (current == NULL || current->next == NULL)
+	{
+		return;
+	}
+	while (current != NULL)
+	{
+		while ((current->prev != NULL) && (current->prev->n > current->n))
+		{
+			tmp = current->prev;
+			current->prev = tmp->prev;
+			tmp->next = current->next;
+			if (current->next)
+			{
+				current->next->prev = tmp;
+			}
+			current->next = tmp;
+			tmp->prev = current;
+			if (current->prev != NULL)
+			{
+				current->prev->next = current;
+			}
+			else
+			{
+				*list = current;
+			}
+			print_list(*list);
+		}
+		current = current->next;
+
+	}
 }
